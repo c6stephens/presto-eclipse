c60bc4275e72eb92ca2ec7be10f84c10680352f4
Chris Stephens <cstephens@netflix.com>
2014-08-14 11:33:31 -0700

    moved to generic UI

---
+++ plugins/org.eclipse.datatools.enablement.presto.ui/src/org/eclipse/datatools/enablement/presto/ui/IPrestoConnectionProfileConstants.java
@@ -0,0 +1,11 @@
+package org.eclipse.datatools.enablement.presto.ui;
+
+/**
+ * @author brianf
+ *
+ */
+public interface IPrestoConnectionProfileConstants {
+
+	public static final String Presto_CATEGORY_ID = "org.eclipse.datatools.enablement.presto.driver.category"; //$NON-NLS-1$
+	
+}
